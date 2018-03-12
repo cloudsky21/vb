c2307f0172ddfecae857e7fbb1720f120f28e72d
clouddyy <32190333+cloudsky21@users.noreply.github.com>
2018-03-12 13:59:12 +0800

    first commit

---
+++ connectApp/Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports MySql.Data.MySqlClient
+Imports MySql.Data
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        Dim sqladapter As New MySqlDataAdapter
+        Dim sqlcmd As MySqlCommand
+        Dim dr As MySqlDataReader
+        Dim dt As New DataTable
+
+
+
+        Try
+            Connect()
+            sqlcmd = New MySqlCommand("SELECT * FROM users WHERE usrid = 'root'")
+            sqlcmd.Connection = conn
+            dr = sqlcmd.ExecuteReader()
+            dt.Load(dr)
+
+            TextBox1.Text = dt.Rows(0).Item(0).ToString()
+
+        Catch ex As MysqlException
+            MsgBox("Error:" & ex.Message)
+
+        End Try
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ connectApp/Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports MySql.Data.MySqlClient
+Module ConnectMySQL
+    Public conn As New MySql.Data.MySqlClient.MySqlConnection
+    Public Sub Connect()
+        Dim DatabaseName As String = "livestock"
+        Dim Server As String = "localhost"
+        Dim UserName As String = "root "
+        Dim Password As String = "pinipirawako100"
+        If Not conn Is Nothing Then conn.Close()
+        conn.ConnectionString = String.Format("server={0}; user id={1}; password={2}; database={3}; pooling=false", Server, UserName, Password, DatabaseName)
+        Try
+            conn.Open()
+            MsgBox(conn.State.ToString)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Module
